f96f3adcc9471f054eb68af3d15c1bebcd89689e
E Jikan <94772817+vstreame@users.noreply.github.com>
2024-11-01 10:44:51 -0700

    feat: Use agent to start showing data

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,9 @@
-import type { MetaFunction } from "@remix-run/node";
+import {
+  json,
+  type LoaderFunctionArgs,
+  type MetaFunction,
+} from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,6 +14,27 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function loader({ request, context }: LoaderFunctionArgs) {
+  const agent = await context.maybeLoggedInUser(request);
+
+  if (agent) {
+    const { data: profile } = await agent.app.bsky.actor.getProfile(
+      { actor: agent.assertDid },
+      { signal: request.signal },
+    );
+    return json({ profile });
+  }
+
+  return json({ profile: null });
+}
+
 export default function Index() {
-  return <h1>VStream</h1>;
+  const { profile } = useLoaderData<typeof loader>();
+
+  return (
+    <>
+      <h1>VStream</h1>
+      {profile ? <pre>{JSON.stringify(profile, null, 2)}</pre> : null}
+    </>
+  );
 }
