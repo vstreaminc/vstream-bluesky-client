40e3685e98246ff7fe2bd4c300ca79ddae8f204d
E Jikan <94772817+vstreame@users.noreply.github.com>
2024-11-06 10:24:00 -0800

    feat: Add homepage meta

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,14 +1,96 @@
-import { type MetaFunction } from "@remix-run/node";
+import type { MetaDescriptor, MetaFunction } from "@remix-run/node";
+import type { Organization, WithContext } from "schema-dts";
 import { MainLayout } from "~/components/mainLayout";
 import { useViewer } from "~/hooks/useViewer";
+import { PRODUCT_NAME } from "~/lib/constants";
+import { metaIntl } from "~/lib/locale.server";
+import logoSvg from "~/imgs/logo.svg";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "VStream" },
+export const meta: MetaFunction = (args) => {
+  const t = metaIntl(args);
+
+  const metas: MetaDescriptor[] = [
+    {
+      title: t.formatMessage(
+        {
+          defaultMessage: "Home | {productName}",
+          description: "Title for the home page of website",
+        },
+        { productName: PRODUCT_NAME },
+      ),
+    },
     { name: "description", content: "~~~Under Construction~~~" },
     // TODO: Remove before going live
     { name: "robots", content: "noindex" },
   ];
+
+  // https://developers.google.com/search/docs/appearance/structured-data/sitelinks-searchbox
+  // https://developers.google.com/search/docs/appearance/site-names
+  // https://schema.org/WebPage
+  // TODO: Bring this back when search is implmented
+  // const ldWebSite: WithContext<WebSite> = {
+  //   "@context": "https://schema.org",
+  //   "@type": "WebSite",
+  //   name: PRODUCT_NAME,
+  //   potentialAction: {
+  //     "@type": "SearchAction",
+  //     target: {
+  //       "@type": "EntryPoint",
+  //       urlTemplate: "https://vstream.com/search?q={search_term_string}",
+  //     },
+  //     // @ts-expect-error Does not know this is required
+  //     "query-input": "required name=search_term_string",
+  //   },
+  //   url: "https://vstream.com",
+  // };
+
+  // https://developers.google.com/search/docs/appearance/structured-data/logo
+  // https://schema.org/Organization
+  const ldOrg: WithContext<Organization> = {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    founders: [
+      {
+        "@type": "Person",
+        familyName: "Koslow",
+        givenName: "Eric",
+        image:
+          "https://pbs.twimg.com/profile_images/1540529950939156480/bet_J3yK_400x400.jpg",
+        jobTitle: "CEO",
+        name: "Eric Koslow",
+      },
+      {
+        "@type": "Person",
+        familyName: "Jikan",
+        givenName: "E",
+        image:
+          "https://pbs.twimg.com/profile_images/1497678405478670339/cLcWqzUl_400x400.jpg",
+        name: "E Jikan",
+      },
+    ],
+    foundingDate: "2022-04-04",
+    foundingLocation: {
+      "@type": "Place",
+      address: {
+        "@type": "PostalAddress",
+        addressCountry: "US",
+        addressRegion: "Califonia",
+        addressLocality: "San Francisco",
+      },
+    },
+    logo: "https://vstream.com" + logoSvg,
+    name: PRODUCT_NAME,
+    sameAs: [
+      "https://twitter.com/vstream_en",
+      "https://twitter.com/vstream_jp",
+      "https://www.linkedin.com/company/vstreaminc",
+    ],
+    url: "https://vstream.com",
+  };
+
+  metas.push({ "script:ld+json": [/* ldWebSite, */ ldOrg] });
+
+  return metas;
 };
 
 export default function Index() {
@@ -16,7 +98,7 @@ export default function Index() {
 
   return (
     <MainLayout>
-      <h1>VStream</h1>
+      <h1>{PRODUCT_NAME}</h1>
       {viewer ? <pre>{JSON.stringify(viewer, null, 2)}</pre> : null}
     </MainLayout>
   );
