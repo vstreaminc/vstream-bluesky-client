feda063be87f1594135e02d8f86dfd49fc9f253a
E Jikan <94772817+vstreame@users.noreply.github.com>
2024-11-28 09:46:40 -0800

    feat: Update URL & handle detection

--- app/components/descriptionAutoLinker.tsx
+++ app/components/descriptionAutoLinker.tsx
@@ -1,10 +1,13 @@
 import { Fragment, memo } from "react";
 import { Link } from "react-aria-components";
+import { $path } from "remix-routes";
 
-const URL_REGEX = /(https?:\/\/[^\s]+)/g;
+const URL_REGEX =
+  /((?:https?:\/\/)?(?:(?:[a-z0-9]?(?:[a-z0-9-]{1,61}[a-z0-9])?\.[^.|\s])+[a-z.]*[a-z]+|(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3})(?::\d{1,5})*[a-zA-Z0-9.,@_/~#&=;%+?\-\\(\\)]*)/g;
 const HASH_REGEX = /(#[0-9a-zA-Z_]+)/g;
 const NEWLINE_REGEX = /(\n)/g;
-const HANDLE_REGEX = /\s(@[[0-9a-zA-Z_.]+)|\s([0-9a-zA-Z_]+\.bsky\.social)/g;
+const HANDLE_REGEX =
+  /((?<!\w)@[0-9a-zA-Z_]+\.[0-9a-zA-Z_]+\.[0-9a-zA-Z_]+)|(\w+\.bsky\.social)/g;
 const MASTER_REGEX = new RegExp(
   [URL_REGEX, HASH_REGEX, NEWLINE_REGEX, HANDLE_REGEX]
     .map((x) => x.source)
@@ -27,11 +30,11 @@ export const DescriptionAutoLinker = memo(function DescriptionAutoLinker({
         } else if (URL_REGEX.test(word)) {
           return (
             <Link
-              className="text-blue-400"
               key={idx}
-              href={word}
-              target="_blank"
+              className="text-blue-400"
+              href={word.startsWith("http") ? word : `https://${word}`}
               rel="noopener noreferrer"
+              target="_blank"
             >
               {word}
             </Link>
@@ -44,9 +47,13 @@ export const DescriptionAutoLinker = memo(function DescriptionAutoLinker({
           );
         } else if (HANDLE_REGEX.test(word)) {
           return (
-            <span key={idx} className="cursor-pointer text-blue-400">
+            <Link
+              key={idx}
+              className="text-blue-400"
+              href={$path("/c/:handle", { handle: word })}
+            >
               {word}
-            </span>
+            </Link>
           );
         }
 
